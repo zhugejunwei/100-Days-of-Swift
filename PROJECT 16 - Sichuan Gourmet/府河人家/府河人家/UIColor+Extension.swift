a0b476b76245cef6e5ea2a80e4d489c6ae683636
Junwei Zhuge <zhugerock@gmail.com>
2016-11-13 09:49:55 -0500

    search ui

--- PROJECT 16 - Sichuan Gourmet/府河人家/府河人家/UIColor+Extension.swift
+++ PROJECT 16 - Sichuan Gourmet/府河人家/府河人家/UIColor+Extension.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-import UIKit
-
 extension UIColor
 {
     /**
---
+++ PROJECT 17 - SearchCourses/SearchEngine/SearchEngine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchEngine
+//
+//  Created by 诸葛俊伟 on 11/10/16.
+//  Copyright © 2016 University of Pittsburgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
